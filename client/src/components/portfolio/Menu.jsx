90edc64fba353e0f213f21c03fdd15d39f167bd4
pruthviraj-gaikwad <pruthviraj162004@gmail.com>
2025-08-12 16:59:49 +0530

    chat application added

--- client/src/components/portfolio/Menu.jsx
+++ client/src/components/portfolio/Menu.jsx
@@ -2,6 +2,7 @@ import Work1 from "../../assets/movie_booking_application.png";
 import Work2 from "../../assets/DragonBall.png";
 import Work3 from "../../assets/qrcode.png";
 import Work4 from "../../assets/todolist.png";
+import work1 from "../../assets/chatapp.png";
 
 const Menu = [
 	{
@@ -14,6 +15,14 @@ const Menu = [
 	},
 	{
 		id: 2,
+		image: work1,
+		title: "ChatApp",
+		category: ["Frontend", "React", "Backend"],
+		url: "https://chatapp-seven-rust.vercel.app/",
+		repositoryUrl: "https://github.com/pruthviraj-gaikwad/chat_app_using_reactNode.js",
+	},
+	{
+		id: 3,
 		image: Work2,
 		title: "Dragon Ball",
 		category: ["Frontend"],
@@ -21,7 +30,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/pruthviraj-gaikwad/jsProject"
 	},
 	{
-		id: 3,
+		id: 4,
 		image: Work3,
 		title: "QR-code Generator",
 		category: ["Frontend"],
@@ -29,7 +38,7 @@ const Menu = [
 		repositoryUrl: "https://github.com/pruthviraj-gaikwad/projects/tree/main/QR%20Code"
 	},
 	{
-		id: 4,
+		id: 5,
 		image: Work4,
 		title: "To_Do_List",
 		category: ["Frontend"],
